a966be07f166afb672add3f5642a18b6e1820932
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-08-22 21:18:24 -0400

    homework

---
+++ halfint.java
@@ -0,0 +1,14 @@
+public class Halfint{
+
+public double halfInt(int c)
+{
+    
+   return c/2.0;
+}
+
+public static void main(String[] args) {
+    Halfint h = new Halfint();
+    System.out.println(h.halfInt(4));
+}
+}
+
